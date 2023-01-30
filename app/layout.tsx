31c69c953e9f0ea3e9e4d473d3689bd9df4dd8a2
AM Codes <amccodes@gmail.com>
2023-01-30 23:21:39 +0700

    Create Project companu progile agun interior

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './globals.css';
+
+// import Font Awesome CSS
+import "@fortawesome/fontawesome-svg-core/styles.css"; 
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Tell Font Awesome to skip adding the CSS automatically 
+// since it's already imported above
+config.autoAddCss = false; 
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
