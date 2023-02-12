9c847a68995652675c001246ba97755aec813db5
AM Codes <amccodes@gmail.com>
2023-02-12 22:07:27 +0700

    Change layout profile and office

--- app/office/page.tsx
+++ app/office/page.tsx
@@ -1,8 +1,19 @@
 export default function Office() {
   return (
     <>
-      <div className="container">
-        <h1>Office</h1>
+      <div className="container my-5" style={{border: "2px solid #efb733", borderRadius: "5px"}}>
+        <div className="row py-5">
+          <div className="col-md-6 col-sm-12 col-xs-12">
+            <h2>PT. AGUN INTERIOR GROUP</h2>
+            Jln. Raya Banjar - Sidaharja<br/>
+            Lingk. Sukamanah, Kec. Pataruman, Kel. Pataruman<br/>
+            Kota Banjar, Jawa Barat<br/><br/>
+          </div>
+
+          <div className="col-md-6 col-sm-12 col-xs-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.859516826376!2d108.56454711487439!3d-7.36963889468216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f6263d21e8a7b%3A0x926d50874ce819b!2sAGUN%20INTERIOR!5e0!3m2!1sen!2sid!4v1676212377500!5m2!1sen!2sid" width="100%" height="300" style={{border: "2px solid #efb733", borderRadius: "5px"}} loading="lazy"></iframe>
+          </div>
+        </div>
       </div>
     </>
   )
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,42 @@
 export default function Profile() {
   return (
     <>
-      <div className="container">
-        <h1>Profile</h1>
+      <div className="container" style={{backgroundImage: "url('/banner-profile.png')", width:"100%", height: "auto", color:"#fff", borderRadius: "5px"}}>
+        <div className="row">
+          <div className="col-12 py-5">
+            <center className="lh-lg">
+              <h1><u>AGUN INTERIOR</u></h1>
+              <p>
+                Perusahaan kami bergerak dibidang barang dan jasa interior dan exterior serta properti untuk berjualan<br/>
+                Produk unggulan yang selalu di minati oleh semua kalangan<br/>
+                Properti yang selalu di butuhkan oleh rumah dan tempat usaha anda<br/>
+                Nyaman untuk di lihat dan lebih elegan dengan di padukan dengan gaya modern
+              </p>
+            </center>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="container my-5" style={{border: "2px solid #efb733", borderRadius: "5px"}}>
+        <div className="row py-5">
+          <div className="col-md-6 col-sm-12 col-xs-12">
+            <h2>Visi</h2>
+            Menjadi perusahaan interior dan exterior yang terdepan, bekerja secara professional, berintegritas terpercaya dan selalu inovatif.<br/><br/><br/>
+            
+            <h2>Misi</h2>
+            Berkomitmen untuk menghasilkan produk interior dan exterior yang bermutu, dengan berorientasi kepada kepuasan pelanggan, terus menerus untuk berinovasi dan berkembang secara berkesinambungan.<br/><br/>
+          </div>
+
+          <div className="col-md-6 col-sm-12 col-xs-12">
+            <div style={{border: "2px solid #efb733", borderRadius: "5px", margin: "0.5rem", padding:"2rem 1rem "}}>
+              <h2>Kontak Kami</h2><br/>
+              <b>WhatsApp :</b> 085 722 792 010<br/><br/>
+              <b>Facebook :</b> Agun Interior & Exterior<br/><br/>
+              <b>Instagram :</b> agun.interior.exterior<br/><br/>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
