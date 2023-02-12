b98059b9843b51b2cef7aac577b5b03219140541
AM Codes <amccodes@gmail.com>
2023-02-12 20:49:38 +0700

    Change navigation active and style image logo header

--- app/common/header.tsx
+++ app/common/header.tsx
@@ -3,8 +3,11 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
@@ -13,14 +16,15 @@ export default function Header() {
             alt="logo"
             layout='responsive'
             height="70"
-            width="380"/>
+            width="380"
+            style={{maxHeight:"2.5rem"}}/>
       </a>
 
       <ul className="nav nav-pills">
-        <li className="nav-item"><a href="/" className={`nav-link`} aria-current="page">Home</a></li>
-        <li className="nav-item"><Link href="/profile" className={`nav-link`}>Profile</Link></li>
-        <li className="nav-item"><Link href="/office" className={`nav-link`}>Office</Link></li>
-        <li className="nav-item"><Link href="/gallery" className={`nav-link`}>Gallery</Link></li>
+        <li className="nav-item"><a href="/" className={pathname == "/" ? "nav-link active" : "nav-link"} aria-current="page">Home</a></li>
+        <li className="nav-item"><Link href="/profile" className={pathname == "/profile" ? "nav-link active" : "nav-link"}>Profile</Link></li>
+        <li className="nav-item"><Link href="/office" className={pathname == "/office" ? "nav-link active" : "nav-link"}>Office</Link></li>
+        <li className="nav-item"><Link href="/gallery" className={pathname == "/gallery" ? "nav-link active" : "nav-link"}>Gallery</Link></li>
       </ul>
     </header>
   )
